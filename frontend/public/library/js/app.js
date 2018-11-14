c6092557940aeeb1a5a362ec6f3dde6602335100
Alex Sumner <alex.sumner@realitymine.com>
2018-11-14 18:47:04 +0000

    Fix js

--- frontend/public/library/js/app.js
+++ frontend/public/library/js/app.js
@@ -1,13 +1,14 @@
 var vm = new Vue({
   data: {
     // declare message with an empty value
-    reservations: {[
-      Instances: []
-    ]}
-
+    reservations: {},
+    reservation: {
+        Instances: []
+    }
   },
   el: '#app'
 })
+
 // set `message` later
 Vue.http.get('https://6o3l4nfc7a.execute-api.eu-west-1.amazonaws.com/dev/ec2').then(function(response){
   vm.reservations = response.body.Reservations;
