bb691b50e4e6027c933e6619f176b25490458479
Alex Sumner <alex.sumner@realitymine.com>
2018-11-14 18:12:24 +0000

    Change handler

--- backend/handler.js
+++ backend/handler.js
@@ -8,6 +8,10 @@ module.exports.getInstances = async (event, context, callback) => {
     var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify(await ec2.describeInstances().promise())
+        body: JSON.stringify(await ec2.describeInstances().promise()),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+        }
     });
 };
