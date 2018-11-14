44769c099f8752e208b759e12f16c16a0b2c1382
Alexander Harris <mcalapurge@techie.com>
2018-11-14 18:11:53 +0000

    not working

--- frontend/public/library/js/app.js
+++ frontend/public/library/js/app.js
@@ -1,6 +1,16 @@
-var app = new Vue({
-  el: '#app',
+var vm = new Vue({
   data: {
-    message: 'Hello Vue!'
-  }
+    // declare message with an empty value
+    reservations: {[
+      Instances: []
+    ]}
+
+  },
+  el: '#app'
 })
+// set `message` later
+Vue.http.get('https://6o3l4nfc7a.execute-api.eu-west-1.amazonaws.com/dev/ec2').then(function(response){
+  vm.reservations = response.body.Reservations;
+  console.log(vm.reservations)
+});
+
